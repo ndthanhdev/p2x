73487e1cb14a70ec23fb5921ac27d15f47f0df61
ThanhTDT <thanhtdt@outlook.com>
2017-09-11 22:07:40 +0700

    .

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace App
 {
+
+
     class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("Kernel32")]
+        private static extern IntPtr GetConsoleWindow();
+
         /// <summary>
         /// 
         /// </summary>
@@ -17,10 +26,14 @@ namespace App
         /// 2: number of relay on left side
         /// 3: server url
         /// 4: secret
-        /// 5: debug mode or production mode (for prouction mode console app will be hidden)
+        /// 5: 0 = Hidden window ( for production), 5 = Show window (more info https://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx)
         /// </param>
         static void Main(string[] args)
         {
+            IntPtr hwnd;
+            hwnd = GetConsoleWindow();
+            ShowWindow(hwnd, int.Parse(args[5]));
+
             // for debug
             FakeHldMainBoard.HldMainBoard.Path = "board.txt";
 
