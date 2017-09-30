0f6c0dad87e95256bb75627637ba28e85b78649e
ThanhTDT <thanhtdt@outlook.com>
2017-10-01 03:25:08 +0700

    forece open

--- backend/src/graphql/queries/Safe/forceOpenSafe.ts
+++ backend/src/graphql/queries/Safe/forceOpenSafe.ts
@@ -1,4 +1,6 @@
 import { GraphQLFieldConfig, GraphQLInt, GraphQLString, GraphQLBoolean } from "graphql";
+import * as socket from "../../../socket";
+import { KioskModel, IKiosk } from "../../../models/Kiosk";
 
 export const forceOpenSafe: GraphQLFieldConfig<any, any> = {
     type: GraphQLBoolean,
@@ -11,7 +13,20 @@ export const forceOpenSafe: GraphQLFieldConfig<any, any> = {
         }
     },
     resolve: async (source, args, context, info) => {
-        // implement
-        return false;
+        try {
+            const model = await KioskModel.findOne(<IKiosk>{ IC: args.ic }).exec();
+            if (!model) {
+                throw `Kiosk with IC ${args.ic}`;
+            }
+
+            const index = model.Safes.findIndex((value) => value.no === args.no);
+            if (index < 0) {
+                throw `There's not Safe ${args.no} on Kiosk ${args.ic}`;
+            }
+
+            return socket.openSafe(args.ic, args.no);
+        } catch (error) {
+            throw error;
+        }
     }
 };
